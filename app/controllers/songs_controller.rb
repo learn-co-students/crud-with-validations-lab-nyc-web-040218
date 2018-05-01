fe5c0056dca3ca4bbfbe18fb604ce7ecfbdee3d2
github username <github email address>
2018-05-01 18:32:31 -0400

    half way there

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,40 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    song = Song.new(post_params(:title, :released, :release_year, :artist_name, :genre))
+    if song.save
+      redirect_to song
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(param[:id])
+  end
+
+  def delete
+    Song.find(params[:id]).destroy
+    redirect_to songs_url
+  end
+
+  private
+  def post_params(*args)
+    params.require(:song).permit(*args)
+  end
+end
---
+++ app/models/song.rb
@@ -0,0 +1,17 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :release_year, message: "cannot have the same title in the same year"}
+  validates :released, inclusion: { in: [true, false]}
+  validates :release_year, presence: true, allow_blank: true, if: :not_released
+  validates :release_year, presence: true, if: :yes_released
+  validates :release_year, numericality: { less_than_or_equal_to: Time.now.year}
+  validates :artist_name, presence: true
+
+  def not_released
+    released == false
+  end
+
+  def yes_released
+    released == true
+  end
+end
