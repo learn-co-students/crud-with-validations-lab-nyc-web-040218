44e02684fcf3f242ca2f6b0a2906c50ebbf6439c
briecodes <hello@brie.work>
2018-04-30 21:06:39 -0400

    validator saved.

--- app/models/my_validator.rb
+++ app/models/my_validator.rb
@@ -1,5 +1,8 @@
 class MyValidator < ActiveModel::Validator
     def validate(record)
+      if :released == nil
+        
+      end
         unless !record.title.blank? && str.any? {|word| record.title.include?(word)}
           record.errors[:name] << false
         end
