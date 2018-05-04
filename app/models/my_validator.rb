9a36ad844a0da6830777e36d934a283458179eb7
briecodes <hello@brie.work>
2018-05-04 13:58:30 -0400

    validator setup, work continues…

--- app/models/my_validator.rb
+++ app/models/my_validator.rb
@@ -1,10 +1,37 @@
 class MyValidator < ActiveModel::Validator
-    def validate(record)
-      if :released == nil
-        
+  def validate(record)
+    
+    # IF :RELEASED IS TRUE, :RELEASE_YEAR MUST EXIST.
+    # :RELEASE_YEAR CANNOT BE IN THE FUTURE.
+    if record[:released] == true
+      if !record[:release_year] || record[:release_year] > Time.new.year
+        record.errors.add :base, 'This record is invalid'
       end
-        unless !record.title.blank? && str.any? {|word| record.title.include?(word)}
-          record.errors[:name] << false
-        end
+    end
+
+    # SETUP FOR CHECKING IF ARTIST HAS ALREADY
+    # BEEN RELEASED THIS SONG IN THIS YEAR
+
+    arr = [record[:release_year], record[:title], record[:artist_name]]
+    results = []
+    results << Song.all.select do |song|
+      if [song.release_year, song.title, song.artist_name] == arr
+        true
+      else
+        false
       end
+    end.flatten!
+
+    if results.include?(true)
+      record.errors.add :base, 'This record is invalid'
+    elsif results.empty? || results[0] == nil
+    else
+    end
+
+
+    
+    # unless !record.title.blank? && str.any? {|word| record.title.include?(word)}
+    #   record.errors[:name] << false
+    # end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -4,9 +4,9 @@ class Song < ActiveRecord::Base
     validates :genre, presence: true
     validates :released, inclusion: { in: [true, false] }
 
-    if validates :released, inclusion: { in: [true] }
-        validates :release_year, presence: true
-    end
+    # if validates :released, inclusion: { in: [true] }
+    #     validates :release_year, presence: true
+    # end
     # MY VALIDATOR TO CHECK IF STATEMENT
-    # validates_with MyValidator
+    validates_with MyValidator
 end
